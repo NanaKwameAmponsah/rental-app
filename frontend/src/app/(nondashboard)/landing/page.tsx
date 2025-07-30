9f07e79fae95b4138d2eb408e6304f5527fd90a9
Nana Kwame Amponsah <nkwame11604@gmail.com>
2025-07-30 19:02:20 -0400

    feature section of landing page done

--- frontend/src/app/(nondashboard)/landing/page.tsx
+++ frontend/src/app/(nondashboard)/landing/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import HeroSection from './HeroSection';
+import FeatureSection from './FeatureSection';
 
 const Landing = () => {
-  return <div><HeroSection/></div>;
+  return <div>
+    <HeroSection/>
+    <FeatureSection />
+    </div>;
 };
 
 export default Landing
