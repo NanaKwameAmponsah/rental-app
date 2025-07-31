6d1cf3479dee28d45a916e290e3a644c55032241
Nana Kwame Amponsah <nkwame11604@gmail.com>
2025-07-30 20:23:25 -0400

    call to action section done

--- frontend/src/app/(nondashboard)/landing/page.tsx
+++ frontend/src/app/(nondashboard)/landing/page.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import HeroSection from './HeroSection';
 import FeatureSection from './FeatureSection';
 import DiscoverSection from './DiscoverSection';
+import CallToActionSection from './CallToActionSection';
 
 const Landing = () => {
   return <div>
     <HeroSection/>
     <FeatureSection/>
     <DiscoverSection/>
-    </div>;
+    <CallToActionSection/>
+    </div>
 };
 
 export default Landing
