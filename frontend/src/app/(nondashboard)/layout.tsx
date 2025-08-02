6976a77ac4f99546cebdda0f397f2608e2e50c8b
Nana Kwame Amponsah <nkwame11604@gmail.com>
2025-08-02 14:08:06 -0400

    tenant settings and manager settings done!

--- frontend/src/app/(nondashboard)/layout.tsx
+++ frontend/src/app/(nondashboard)/layout.tsx
@@ -1,18 +1,18 @@
 "use client";
-import Navbar from '@/components/Navbar';
-import { NAVBAR_HEIGHT } from '@/lib/constants';
+
+import Navbar from "@/components/Navbar";
+import { NAVBAR_HEIGHT } from "@/lib/constants";
 import { useGetAuthUserQuery } from "@/state/api";
 import { usePathname, useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
-
-const Layout = ({ children }: { children: React.ReactNode } ) => {
-  const { data: authUser, isLoading: authLoading } = useGetAuthUserQuery();
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { data: authUser } = useGetAuthUserQuery();
   const router = useRouter();
   const pathname = usePathname();
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    // Only redirect if user is authenticated and trying to access certain paths
     if (authUser) {
       const userRole = authUser.userRole?.toLowerCase();
       if (
@@ -20,19 +20,17 @@ const Layout = ({ children }: { children: React.ReactNode } ) => {
         (userRole === "manager" && pathname === "/")
       ) {
         router.push("/managers/properties", { scroll: false });
-      } else {
-        setIsLoading(false);
       }
     }
   }, [authUser, router, pathname]);
 
-  if (authLoading || isLoading) return <>Loading...</>;
   return (
     <div className="h-full w-full">
       <Navbar />
-      <main className={`h-full flex w-full flex-col`}
+      <main
+        className={`h-full flex w-full flex-col`}
         style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}
-        >
+      >
         {children}
       </main>
     </div>
