066cc56ef0acda4a68800fe58d7e42edadceb909
Nana Kwame Amponsah <nkwame11604@gmail.com>
2025-08-03 10:58:17 -0400

    search filters, mapbox api integration, listings pages done

--- backend/dist/src/index.js
+++ backend/dist/src/index.js
@@ -13,7 +13,8 @@ const tenantRoutes_1 = __importDefault(require("./routes/tenantRoutes"));
 const managerRoutes_1 = __importDefault(require("./routes/managerRoutes"));
 const propertyRoutes_1 = __importDefault(require("./routes/propertyRoutes"));
 const leaseRoutes_1 = __importDefault(require("./routes/leaseRoutes"));
-const applicationRoutes_1 = __importDefault(require("./routes/applicationRoutes"));
+const applicationRoute_1 = __importDefault(require("./routes/applicationRoute"));
+const authMiddleware_1 = require("./Middleware/authMiddleware");
 /*CONFIGURATIONS */
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -28,11 +29,11 @@ app.use((0, cors_1.default)());
 app.get("/", (req, res) => {
     res.send("This is home route");
 });
-app.use("/applications", applicationRoutes_1.default);
+app.use("/applications", applicationRoute_1.default);
 app.use("/properties", propertyRoutes_1.default);
 app.use("/leases", leaseRoutes_1.default);
-app.use("/tenants", authMiddleware(["tenant"]), tenantRoutes_1.default);
-app.use("/managers", authMiddleware(["manager"]), managerRoutes_1.default);
+app.use("/tenants", (0, authMiddleware_1.authMiddleware)(["tenant"]), tenantRoutes_1.default);
+app.use("/managers", (0, authMiddleware_1.authMiddleware)(["manager"]), managerRoutes_1.default);
 /* SERVER */
 const port = Number(process.env.PORT) || 3002;
 app.listen(port, "0.0.0.0", () => {
--- backend/dist/src/routes/applicationRoute.js
+++ backend/dist/src/routes/applicationRoute.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const authMiddleware_1 = require("../middleware/authMiddleware");
+const authMiddleware_1 = require("../Middleware/authMiddleware");
 const applicationControllers_1 = require("../controllers/applicationControllers");
 const router = express_1.default.Router();
 router.post("/", (0, authMiddleware_1.authMiddleware)(["tenant"]), applicationControllers_1.createApplication);
